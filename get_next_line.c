7a07d9df430e4223d730d2070482bb70ce05a743
Theo LEVAUFRE <tlevaufr@e1r10p6.42.fr>
2018-01-15 21:00:19 +0100

    fonction New_or_find 1ere version

--- get_next_line.c
+++ get_next_line.c
@@ -1,31 +1,43 @@
 #include "get_next_line.h"
 
-t_gnl_lst	*list;
 
-char		**concat_line(int fd, char **line)
+char		**concat_line(int fd, t_gnl_lst *file, char **line_adress)
 {
+	char	*line;
+	while (read(fd, file->buf, BUFF_SIZE)
 }
-t_gnl_lst	*scan_list(int fd, 
-
-t_gnl_lst	*new_list(int fd)
+t_gnl_lst	*new_or_find(const int fd, t_gnl_lst **list, int unfound)
 {
 	t_gnl_lst	*ptr;
-
-	if (!(ptr = malloc(sizeof(t_gnl_lst))))
-		return (NULL);
-	ptr->fd = fd;
-	read(fd, ptr->buf, [BUFF_SIZE]);
-	ptr->bytes_readed = [BUFF_SIZE];
-	ptr->next = NULL;
-	return (ptr);
+	if (!list || unfound == 1)
+	{
+		if (!(ptr = malloc(sizeof(t_gnl_lst))))
+			return (NULL);
+		ptr->fd = fd;
+		//read(fd, ptr->buf, BUFF_SIZE);
+		ptr->bytes_readed = 0;
+		ptr->next = unfound ? *list : NULL;
+		list = &ptr;
+		return (*list);
+	}
+	else
+	{
+		ptr = *list;
+		while (ptr->next && ptr->fd != fd)
+			ptr = ptr-> next;
+		if (fd == ptr->fd)
+			return (ptr);
+		return (new_or_find(fd, list, 1));
+	}
 }
 
-int			get_next_line(int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
-	if (!line)
+	static t_gnl_lst	**list;
+
+	if (!*line)
 		return (-1);
-	if (!list)		&list = new_list(fd);
-	
+	new_or_find(fd, list, 0);
 }
 
 int
