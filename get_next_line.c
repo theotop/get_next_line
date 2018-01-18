6b6eacf2611eb15c3a2313e53a655a360e7119cd
Theo LEVAUFRE <tlevaufr@e1r10p8.42.fr>
2018-01-18 19:24:22 +0100

    hop

--- get_next_line.c
+++ get_next_line.c
@@ -1,20 +1,56 @@
 #include "get_next_line.h"
 
+size_t		size_to_write(t_gnl_lst *file, int read_value)
+{
+	size_t	nb_of_chars;
+	size_t	i;
 
-char		**concat_line(t_gnl_lst *file, char **line_adress)
+	nb_of_chars = 0;
+	i = file->bytes_readed;
+	while (!ft_issep(file->buf[i], "\n\0" || i < read_value))
+	{
+		nb_of_chars++;
+		i++;
+	}
+	return (nb_of_chars);
+}
+
+char		**concat_line(t_gnl_lst *file, char **line_adress, int read_value)
+{
+	char	*new_line;
+
+		if (!new_line = strnew(size_to_write(file, read_value) + 1))
+			return (NULL);
+	while (!ft_issep(file->buf[file->bytes_readed]) || file->bytes_readed < BUFF_SIZE)
+		new_line = file_buf[file->bytes_readed++]
+	*(new_line + 1) = '\0';
+	if (*line_adress)
+	{
+		if (!(*line_adress) = ft_strjoin((*line_adress), new_line))
+			return (NULL);
+		strdel(new_line);
+		return (line_adress);
+	}
+	return (line_adress = &new_line);
+}
+
+char		**make_line(t_gnl_lst *file, char **line_adress)
 {
 	int		bypass_read;
-	char	*line;
-	
+	int		read_value;
+
 	bypass_read = (file->bytes_readed < BUFF_SIZE) ? 1 : 0;
-	while (bypass_read || read(file->fd, file->buf, BUFF_SIZE))
+	while (bypass_read || read_value = read(file->fd, file->buf, BUFF_SIZE))
 	{
-		bytes_readed = bypass_read ? bytes_readed : 0;
-		line = &ft_strndup2(*(file->buf) + bytes_readed, (UNE FONCTION QUI ME 
-			DIT COMBIEN JE DOIS ÉCRIRE, CHECKE LES \N
-			SI ELLE N'EN TROUVE PAS ELLE RENVOIE BUFF_SIZE - bytes_readed))
+		if (read_value == -1)
+
+		file->bytes_readed = bypass_read ? file->bytes_readed : 0;
+		concat_line(file, line_adress, read_value);
+		bypass_read = (file->bytes_readed < BUFF_SIZE) ? 1 : 0;
+		if (
 	}
 }
+
 t_gnl_lst	*new_or_find(const int fd, t_gnl_lst **list, int unfound)
 {
 	t_gnl_lst	*ptr;
@@ -24,7 +60,7 @@ t_gnl_lst	*new_or_find(const int fd, t_gnl_lst **list, int unfound)
 			return (NULL);
 		ptr->fd = fd;
 		//read(fd, ptr->buf, BUFF_SIZE);
-		ptr->bytes_readed = 0;
+		ptr->bytes_readed = BUFF_SIZE;
 		ptr->next = unfound ? *list : NULL;
 		list = &ptr;
 		return (*list);
@@ -43,12 +79,6 @@ int			get_next_line(const int fd, char **line)
 
 	if (!*line)
 		return (-1);
-	concat_line(new_or_find(fd, list, 0), line);
+	make_line(new_or_find(fd, list, 0), line);
 	
 }
-
-
-C\UC
-2
-
-
